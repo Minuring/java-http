65018ed6b202a913e6ec7221868434dff3fcf3a7
Minuring <mw9505@gmail.com>
2025-09-13 14:54:34 +0900

    feat: 404 Not Found 페이지 응답

--- tomcat/src/main/java/com/techcourse/controller/NotFoundController.java
+++ tomcat/src/main/java/com/techcourse/controller/NotFoundController.java
@@ -1,13 +1,21 @@
 package com.techcourse.controller;
 
 import org.apache.coyote.controller.AbstractController;
+import org.apache.coyote.io.StaticResource;
 import org.apache.coyote.message.HttpRequest;
 import org.apache.coyote.message.HttpResponse;
 
 public class NotFoundController extends AbstractController {
 
     @Override
-    public HttpResponse service(final HttpRequest request) {
-        return HttpResponse.notFound().build();
+    public HttpResponse service(final HttpRequest request) throws Exception {
+        final var resource = new StaticResource("/404.html");
+        final var body = resource.readAsString();
+
+        return HttpResponse.notFound()
+                .contentType("text/html;charset=utf-8")
+                .contentLength(body.length())
+                .body(body)
+                .build();
     }
 }
